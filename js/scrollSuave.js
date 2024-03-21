8b247fe888002161290489ab1c0e0a6a6c573d9e
goner <gonzaloalcagamarra@gmail.com>
2024-03-20 23:47:34 -0500

    "se agrego el modal"

--- js/scrollSuave.js
+++ js/scrollSuave.js
@@ -12,10 +12,42 @@ $(document).ready(function () {
         if (position >= 0 && position <= 200) {
             btnVolverArriba.css('margin-right', '-60px');
 
-        } else {
+        } else  {
             btnVolverArriba.css('margin-right', '60px');
         }
 
     })
+    //Movimiento suave del scroll de inicio y volver arriba
+
+    $('a.volver-arriba').on('click', function(e){
+        e.preventDefault();
+
+        if($(window).scrollTop() != 0){
+            $('html, body').stop().animate({scrollTop: 0}, 1000);
+        }
+
+
+    })
+
+    //Movimiento suave de scroll para los demas items del menu principal
+
+    $('a.scroll-suave').on('click', function(e){
+        e.preventDefault();
+        let seccionOffsetTop = $($(this).attr('href')).offset().top
+        $('html, body').stop().animate({scrollTop: seccionOffsetTop}, 1000);
+
+    }); 
+    $('a.scroll-acercaDe').on('click', function(e){
+        e.preventDefault();
+        let seccionOffsetTop = $($(this).attr('href')).offset().top -122
+        $('html, body').stop().animate({scrollTop: seccionOffsetTop}, 1000);
+
+    }); 
+    $('a.scroll-equipo').on('click', function(e){
+        e.preventDefault();
+        let seccionOffsetTop = $($(this).attr('href')).offset().top -68
+        $('html, body').stop().animate({scrollTop: seccionOffsetTop}, 1000);
+
+    }); 
 
 });
